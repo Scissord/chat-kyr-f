b64f579e8d727d009a51100df0808e722dc8d587
nurdaulet <scissxrd@gmail.com>
2024-11-18 19:10:07 +0500

    fix

--- build/assets/index-0TYOiE8A.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-U73ueQgN.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
---
+++ build/assets/index-B1UAYP6O.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B8n1JF4P.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
