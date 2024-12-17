b5855f4df0e689bbd03a7faa4b92314783a0b277
nurdaulet <scissxrd@gmail.com>
2024-12-17 18:56:19 +0500

    fix

--- src/pages/Chats/blocks/Chat/CustomerInfo/Certificates.tsx
+++ src/pages/Chats/blocks/Chat/CustomerInfo/Certificates.tsx
@@ -102,7 +102,7 @@ const Certificates: FC<CertificatesProps> = (props) => {
       </p>
       <p
         className={css.p}
-        onClick={() => handleSetVideo('/videos/pay.mp4')}
+        onClick={() => handleSetVideo('/videos/payment.mp4')}
       >
         3.1 Инструкция счета на оплату
       </p>
